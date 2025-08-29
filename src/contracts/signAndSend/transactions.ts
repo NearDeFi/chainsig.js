6f71dce04d1043849c9320dede76e61493b974ee
Guillermo Alejandro Gallardo Diez <gagdiez@MacBookPro.home>
2025-08-29 15:29:10 +0200

    fix: tests

--- src/contracts/signAndSend/transactions.ts
+++ src/contracts/signAndSend/transactions.ts
@@ -1,19 +1,17 @@
-import { Account } from '@near-js/accounts'
-import { KeyPair } from '@near-js/crypto'
-import { JsonRpcProvider } from '@near-js/providers'
-import { KeyPairSigner } from '@near-js/signers'
+import { type KeyPair } from '@near-js/crypto'
 import { createPublicClient, http } from 'viem'
 
 import * as chainAdapters from '@chain-adapters'
 import { BTCRpcAdapters } from '@chain-adapters/Bitcoin/BTCRpcAdapter'
 import { ChainSignatureContract } from '@contracts/ChainSignatureContract'
-import { DONT_CARE_ACCOUNT_ID } from '@contracts/constants'
 import {
   type Response,
   type BitcoinRequest,
   type CosmosRequest,
   type EVMRequest,
 } from '@contracts/types'
+
+import { getNearAccount } from './utils'
 
 export const EVMTransaction = async (
   req: EVMRequest,
@@ -178,44 +176,4 @@ export const CosmosTransaction = async (
       errorMessage: e instanceof Error ? e.message : String(e),
     }
   }
-}
-
-// Aux function
-type SetConnectionArgs =
-  | {
-      networkId: string
-      accountId: string
-      keyPair: KeyPair
-    }
-  | {
-      networkId: string
-      accountId?: never
-      keyPair?: never
-    }
-
-const getNearAccount = async ({
-  networkId,
-  accountId = DONT_CARE_ACCOUNT_ID,
-  keyPair = KeyPair.fromRandom('ed25519'),
-}: SetConnectionArgs): Promise<Account> => {
-  // Get the RPC URL for the network
-  const rpcUrl = {
-    testnet: 'https://rpc.testnet.near.org',
-    mainnet: 'https://rpc.mainnet.near.org',
-  }[networkId]
-
-  if (!rpcUrl) {
-    throw new Error(`Unsupported network: ${networkId}`)
-  }
-
-  // Create provider using new v2.0.0+ API
-  const provider = new JsonRpcProvider({
-    url: rpcUrl,
-  })
-
-  // Create signer using new v2.0.0+ API
-  const signer = new KeyPairSigner(keyPair)
-
-  // Use Account constructor (accountId, provider, signer)
-  return new Account(accountId, provider, signer)
 }
